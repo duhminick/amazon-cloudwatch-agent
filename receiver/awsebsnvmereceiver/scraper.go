e377c2408d476303e645eb9ae10f003027863a73
Dominic Sciascia <dscia@amazon.com>
2025-03-04 22:49:50 -0500

    scrape metrics

--- receiver/awsebsnvmereceiver/scraper.go
+++ receiver/awsebsnvmereceiver/scraper.go
@@ -5,11 +5,13 @@ package awsebsnvmereceiver
 
 import (
 	"context"
+	"fmt"
 	"os"
 	"strings"
 	"time"
 
 	"github.com/aws/amazon-cloudwatch-agent/receiver/awsebsnvmereceiver/internal/metadata"
+	"github.com/aws/amazon-cloudwatch-agent/receiver/awsebsnvmereceiver/internal/nvme"
 	"go.opentelemetry.io/collector/component"
 	"go.opentelemetry.io/collector/pdata/pcommon"
 	"go.opentelemetry.io/collector/pdata/pmetric"
@@ -28,31 +30,106 @@ type nvmeScraper struct {
 	mb     *metadata.MetricsBuilder
 }
 
+type ebsDevice struct {
+	deviceName string
+	devicePath string
+	volumeId   string
+}
+
+type nvmeDeviceFileAttributes struct {
+	controller int
+	namespace int
+	partition int
+}
+
 func (s *nvmeScraper) start(_ context.Context, _ component.Host) error {
-	s.logger.Debug("Starting NVME scraper", zap.String("receiver", metadata.Type.String()))
+	s.logger.Debug("Starting NVMe scraper", zap.String("receiver", metadata.Type.String()))
 	return nil
 }
 
 func (s *nvmeScraper) shutdown(_ context.Context) error {
-	s.logger.Debug("Shutting down NVME scraper", zap.String("receiver", metadata.Type.String()))
+	s.logger.Debug("Shutting down NVMe scraper", zap.String("receiver", metadata.Type.String()))
 	return nil
 }
 
 func (s *nvmeScraper) scrape(ctx context.Context) (pmetric.Metrics, error) {
-	s.logger.Info("[DOMINIC] Running NVME scraper")
+	s.logger.Info("[DOMINIC] Running NVMe scraper")
 
 	now := pcommon.NewTimestampFromTime(time.Now())
 
-	s.mb.RecordTotalReadOpsDataPoint(now, 100)
-	rb := s.mb.NewResourceBuilder()
-	rb.SetVolumeID("vol-1234")
-	s.mb.EmitForResource(metadata.WithResource(rb.Emit()))
+	ebsDevices, err := s.getEbsDevices()
+	if err != nil {
+		return pmetric.NewMetrics(), err
+	}
+
+	for _, device := range ebsDevices {
+		metrics, err := nvme.GetMetrics(device.devicePath)
+		if err != nil {
+			s.logger.Info("unable to get metrics for device", zap.String("device", device.deviceName), zap.Error(err))
+			continue
+		}
+		s.logger.Info("emitting metrics for device", zap.String("device", device.deviceName))
+
+		rb := s.mb.NewResourceBuilder()
+		s.mb.RecordTotalReadOpsDataPoint(now, int64(metrics.ReadOps))
+		rb.SetVolumeID(device.volumeId)
+		s.mb.EmitForResource(metadata.WithResource(rb.Emit()))
+	}
 
 	return s.mb.Emit(), nil
 }
 
+func (s *nvmeScraper) getEbsDevices() (map[string]ebsDevice, error) {
+	allNvmeDevices, err := getNvmeDevices()
+	if err != nil {
+		return nil, err
+	}
+
+	devices := make(map[string]ebsDevice)
+
+	for _, device := range allNvmeDevices {
+		// Only want nvme{id}.
+		// Ignore:
+		// - nvme{controller}n{namespace}
+		// - nvme{controller}n{namespace}p{partition}
+		trimmed := strings.TrimPrefix(device, nvmeDevicePrefix)
+		if !strings.Contains(trimmed, "n") {
+			s.logger.Debug("skipping device because we will likely not have permission", zap.String("device", device))
+			continue
+		}
+
+		serial, err := getNvmeDeviceSerial(device)
+		if err != nil {
+			s.logger.Debug("unable to get serial number of device", zap.String("device", device))
+			continue
+		}
+
+		// Skip if we already have a device we can use
+		if _, ok := devices[serial]; ok {
+			continue
+		}
+
+		if serial[:3] != "vol" {
+			s.logger.Debug("device is not prefixed with vol", zap.String("device", device))
+			continue
+		}
+
+		devices[serial] = ebsDevice{
+			deviceName: device,
+			devicePath: fmt.Sprintf("/dev/%s", device),
+			volumeId:   fmt.Sprintf("vol-%s", serial[2:]),
+		}
+	}
+
+	return devices, nil
+}
+
+func parseNvmeDeviceFileName(device string) (nvmeDeviceFileAttributes, error) {
+	return nvmeDeviceFileAttributes{}, nil
+}
+
 func getNvmeDevices() ([]string, error) {
-	entries, err := os.ReadDir("/dev")
+	entries, err := os.ReadDir(devDirectoryPath)
 	if err != nil {
 		return nil, err
 	}
@@ -68,7 +145,7 @@ func getNvmeDevices() ([]string, error) {
 }
 
 func getNvmeDeviceSerial(device string) (string, error) {
-	data, err := os.ReadFile("/sys/class/nvme/" + device + "/serial")
+	data, err := os.ReadFile(fmt.Sprintf("/sys/class/nvme/%s/serial", device))
 	if err != nil {
 		return "", err
 	}
