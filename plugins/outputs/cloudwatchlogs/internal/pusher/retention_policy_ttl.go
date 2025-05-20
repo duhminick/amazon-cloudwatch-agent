3a7e8185ef0f32d96a98f2d17854148b98f6e480
Dominic Sciascia <dscia@amazon.com>
2025-05-20 18:32:07 -0400

    add old timestamps

--- plugins/outputs/cloudwatchlogs/internal/pusher/retention_policy_ttl.go
+++ plugins/outputs/cloudwatchlogs/internal/pusher/retention_policy_ttl.go
@@ -14,19 +14,23 @@ const (
 )
 
 type retentionPolicyTTL struct {
-	statePath  string
-	timestamps map[string]time.Time
-	mu         sync.RWMutex
-	ch         chan string
-	done       chan struct{}
+	statePath     string
+	// oldTimestamps come from the TTL file on agent start
+	oldTimestamps map[string]time.Time
+	// newTimestamps are the new TTLs that will be saved periodically and when the agent is done
+	newTimestamps map[string]time.Time
+	mu            sync.RWMutex
+	ch            chan string
+	done          chan struct{}
 }
 
 func NewRetentionPolicyTTL(fileStatePath string) *retentionPolicyTTL {
 	r := &retentionPolicyTTL{
-		statePath:  filepath.Join(fileStatePath, logscommon.RetentionPolicyTTLFileName),
-		timestamps: make(map[string]time.Time),
-		ch:         make(chan string, retentionChannelSize),
-		done:       make(chan struct{}),
+		statePath:     filepath.Join(fileStatePath, logscommon.RetentionPolicyTTLFileName),
+		oldTimestamps: make(map[string]time.Time),
+		newTimestamps: make(map[string]time.Time),
+		ch:            make(chan string, retentionChannelSize),
+		done:          make(chan struct{}),
 	}
 
 	r.loadTTLState()
@@ -42,15 +46,8 @@ func (r *retentionPolicyTTL) Done() {
 	<-r.done
 }
 
-// DOMINIC: this should read from the state file when loaded
-// that way when we save, it'll just have the new log groups that we've seen
-// this is for the case when a log group was removed by the user. shouldn't need
-// a mutex then
 func (r *retentionPolicyTTL) IsExpired(group string) bool {
-	r.mu.RLock()
-	defer r.mu.RUnlock()
-
-	if ts, ok := r.timestamps[escapeFilePath(group)]; ok {
+	if ts, ok := r.oldTimestamps[escapeFilePath(group)]; ok {
 		return ts.Add(ttlTime).After(time.Now())
 	}
 	return false
@@ -79,7 +76,7 @@ func (r *retentionPolicyTTL) process() {
 func (r *retentionPolicyTTL) updateTimestamp(group string) {
 	r.mu.Lock()
 	defer r.mu.Unlock()
-	r.timestamps[group] = time.Now()
+	r.newTimestamps[group] = time.Now()
 }
 
 func (r *retentionPolicyTTL) saveState() {
